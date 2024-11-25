87a9666718fdea844e4bb06b37bfa847f6711ba4
Aashish Kumar Ray <65903203+Raysi2002@users.noreply.github.com>
2024-11-25 09:45:28 +0530

    Spring Annotations

---
+++ JavaSpringAnnotations/src/main/java/com/raysi/JavaSpringAnnotationsApplication.java
@@ -0,0 +1,13 @@
+package com.raysi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringAnnotationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringAnnotationsApplication.class, args);
+    }
+
+}
---
+++ JavaSpringAnnotations/src/test/java/com/raysi/JavaSpringAnnotationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raysi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringAnnotationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
