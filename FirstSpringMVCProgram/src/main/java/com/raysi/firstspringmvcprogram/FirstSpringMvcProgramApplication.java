c805dc612a241087214074395e29f78d02b00838
Aashish Kumar Ray <65903203+Raysi2002@users.noreply.github.com>
2024-11-27 18:29:22 +0530

    Spring MVC Module

---
+++ FirstSpringMVCProgram/src/main/java/com/raysi/firstspringmvcprogram/FirstSpringMvcProgramApplication.java
@@ -0,0 +1,13 @@
+package com.raysi.firstspringmvcprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringMvcProgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSpringMvcProgramApplication.class, args);
+    }
+
+}
